5e02d2fb67f06799b9127666b48d6914d78ac5bf
Dean Bate <dean.bate@gmail.com>
2019-11-26 22:28:25 -0700

    130b

--- src/03-objects/scripts/account.js
+++ src/03-objects/scripts/account.js
@@ -1,36 +1,3 @@
-// Consider an ‘Account’. A bank account or investment account. To make life simple there are no interest or bank charges.
-
-// Consider a class called ‘Account’ with the following methods:
-
-// constructor - 2 parms - account name, initial balance
-// deposit - 1 parm - value to be added
-// withdraw -1 parm - value to be withdrawn 
-// balance - no parms - returns current balance 
-
-// In this case, state is only one thing “balance”.  Each method does something to the state of the object. It modifies it or shows it. 
-
-// Methods are verbs or action words. State is typically nouns.  Consider how we would test such an Account. Don’t do this; just consider the following:
-
-// Create an account ‘checkingAccount’ with an initial balance of $25
-// Check the balance to ensure we have $25
-// Deposit $10
-// Check the balance to ensure we have $35
-// Withdraw $30
-// Check the balance to ensure we have $5
-
-// Let’s consider the terminology of this example:
-
-// What is the class? Account
-// What is one of the objects or instances? checkingAccount
-// What are the methods? constructor, deposit, withdraw, balance
-// What are the parameters? to the constructor (accountName, startingBalance), to deposit (amount), to withdraw (amount), to balance() nothing.
-// What is returned from? the constructor - reference to the new object, deposit - nothing, withdraw - nothing, balance - the balance.
-
-    
-// constructor - 2 parms - account name, initial balance
-// deposit - 1 parm - value to be added
-// withdraw -1 parm - value to be withdrawn 
-// balance - no parms - returns current balance 
 
 export class Account {
 
@@ -52,6 +19,37 @@ export class Account {
         return this.currentBalance;
     }
 }
+
+// const viewFunctions = {
+
+//     refreshAccountList: (accounts) => {
+//         //Clear list
+//         while (idAccountList.hasChildNodes()) {
+//             idAccountList.removeChild(idAccountList.firstChild);
+//         }
+//         //Repopulate list
+//         accounts.forEach((account) => {
+//             let newAccount = document.createElement("LI");
+//             newAccount.textContent = `${account.name}: $${account.currentBalance}`;
+//             idAccountList.appendChild(newAccount);
+//         })
+//     },
+
+//     createAccountCard: (accountName, accountBalance) => {
+//         let newCard = document.createElement("DIV");
+//         newCard.id = accountName;
+//         newCard.className = "card";
+
+//         let accountHeader = document.createElement("H3");
+//         accountHeader.textContent = accountName;
+//         newCard.appendChild(accountHeader);
+
+//         viewFunctions.addCardButtons(newCard, accountBalance, accountName);
+
+//         idCardPanel.appendChild(newCard);
+//     },
+
+
 
 // export class AccountController {
 //     constructor() {
