fab9d44e9bbdd0a0ddcabece48197e0dc413f708
Dean Bate <dean.bate@gmail.com>
2019-12-18 16:16:25 -0700

    140A

---
+++ react-02/src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import './App.css';
+import logo from './images/logo.svg';
+import communityIcon from './images/airport.svg';
+import tictactoeIcon from './images/slip.svg';
+import bankIcon from './images/globe.svg';
+import linkIcon from './images/linux.svg';
+
+import Icon from './components/Icon';
+import Starter from './components/starter/Starter';
+// import Game from './components/tictactoe/Game';
+// import AccountsApp from './components/accounts/AccountsApp';
+// import CitiesApp from './components/cities/CitiesApp';
+// import LinkedListApp from './components/linked-list/LinkedListApp';
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      selected: linkIcon
+    }
+
+    this.style = { backgroundColor: '#d4bea7' };
+    this.icons = [logo, tictactoeIcon, bankIcon, communityIcon, linkIcon];
+  }
+
+  onSelect = (event) => {
+  this.setState({
+      selected: event.target.name
+    });
+  }
+
+  
+
+  renderIcons = () => {
+    return this.icons.map((icon) => {
+      return <Icon key={icon} name={icon} image={icon} style={this.state.selected === icon ? this.style : null} onClick={this.onSelect} />
+    })
+  }
+
+  showPage = () => {
+    if (this.state.selected === logo) return <Starter logo={logo}/>;
+    // if (this.state.selected === tictactoeIcon) return <Game />;
+    // if (this.state.selected === bankIcon) return <AccountsApp />;
+    // if (this.state.selected === communityIcon) return <CitiesApp />;
+    // if (this.state.selected === linkIcon) return <LinkedListApp />;
+
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <nav className="nav">
+          {this.renderIcons()}
+        </nav>
+
+        {this.showPage()}
+      </div>
+    );
+  }
+}
+
+
+export default App;
+
+
+
+// import React from 'react';
+// import logo from './logo.svg';
+// import './App.css';
+
+// function App() {
+//   return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
---
+++ react-02/src/App.test.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import Icon from './components/Icon';
+// import Starter from './components/starter/Starter';
+// import Game from './components/tictactoe/Game';
+// import AccountsApp from './components/accounts/AccountsApp';
+// import CitiesApp from './components/cities/CitiesApp';
+
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Icon renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Icon />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Starter renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Starter />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('Tic-tac-toe renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<Game />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('AccountsApp renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<AccountsApp />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
+
+it('CitiesApp renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<CitiesApp />, div);
+});
