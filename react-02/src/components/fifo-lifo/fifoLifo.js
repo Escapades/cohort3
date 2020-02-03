28ca7444548816e2421332ae377d1945b87bb1b6
Dean Bate <dean.bate@gmail.com>
2020-02-02 20:05:34 -0700

    initial spike

---
+++ react-02/src/components/fifo-lifo/fifoLifo.js
@@ -0,0 +1,27 @@
+export class Queue {
+    constructor() {
+        this.queue = [];
+    }
+
+    enqueue(item) {
+        this.queue.push(item);
+    }
+
+    dequeue() {
+        this.queue.shift();
+    }
+}
+
+export class Stack {
+    constructor() {
+        this.stack = [];
+    }
+
+    push(item) {
+        this.stack.push(item);
+    }
+
+    pop() {
+        this.stack.pop();
+    }
+}
